b128d98701e5c913d1147597a9c21cdc89ae8f2e
Charlotte Récapet <22002122+crecapet@users.noreply.github.com>
2019-03-25 12:41:46 +0100

    Updated nmregr_options.m (imported from DEBtool_M)

--- lib/regr/nmregr_options.m
+++ lib/regr/nmregr_options.m
@@ -1,118 +1,157 @@
+%% nmregr_options
+% Sets options for function <nmregr.html *nmregr*>
+
+%%
 function nmregr_options (key, val)
-  %% nmregr_options (key, val)
-  %% created at 2002/02/10 by Bas Kooijman, modified 2005/05/23
-  %% sets options for function 'nmregr' one by one
-  %% type 'nmregr_options' to see values or
-  %% type 'nmregr_options('default') to set options at default values
+  %  created at 2002/02/10 by Bas Kooijman; modified 2015/01/16, 2015/02/27, 2015/07/28 Goncalo Marques
+  
+  %% Syntax
+  % <../nmregr_options .m *nmregr_options*> (key, val)
+  
+  %% Description
+  % Sets options for function 'nmregr' one by one
+  %
+  % Input
+  %
+  % * no input: print values to screen
+  % * one input: 
+  %
+  %    'default' sets options at default values
+  %    other keys (see below) to print value to screen
+  %
+  % * two inputs
+  %
+  %    'report': 1 - to report steps to screen; 0 - not to;
+  %    'max_step_number': maximum number of steps 
+  %    'max_fun_evals': maximum number of function evaluations
+  %    'tol_simplex': tolerance for how close the simplex points must be
+  %       together to call them the same
+  %    'tol_tun': tolerance for how close the loss-function values must be
+  %       together to call them the same
+  %    'simplex_size': fraction added (subtracted if negative) to the free
+  %       parameters when building the simplex
+  %
+  % Output
+  %
+  % * no output, but globals are set to values or values printed to screen
+  
+  %% Example of use
+  % nmregr_options('default'); nmregr_options('report', 0)
 
-  global report max_step_number max_fun_evals tol_simplex tol_fun;
-
-  if exist('key', 'var') == 0
-    key = 'unkown';
-  end
-
-  options = ['default'; 'report'; 'max_step_number'; ...
-	     'max_fun_evals'; 'tol_simplex'; 'tol_fun'; 'unkown'; key];
-  [nr nc ] = size(options); 
-  for i = 1:(nr-2) % determine the option number
-    if nc == sum(options(nr,:) == options(i,:))
-      opt_nr = i;
-      break;
-    end		   
-  end
-  if exist('opt_nr', 'var') == 0 % option is 'other'
-    opt_nr = nc-1;
-  end
-    
-  switch opt_nr
+    global report max_step_number max_fun_evals tol_simplex tol_fun simplex_size
+ 
+    if ~exist('key','var')
+      key = 'inexistent';
+    end
+ 
+    switch key
 	
-   case 1 % option 'default'
-     report = 1;
-     max_step_number = 800;
-     max_fun_evals = 2000;
-     tol_simplex = 1e-4;
-     tol_fun = 1e-4;
-     nmregr_options
+      case 'default'
+	    report = 1;
+	    max_step_number = 500;
+	    max_fun_evals = 2000;
+	    tol_simplex = 1e-4;
+	    tol_fun = 1e-4;
+        simplex_size = 0.05;
 
-   case 2 % option 'report'
-     if exist('val', 'var') == 0
-       if prod(size(report)) == 1
-	     fprintf(['report = ', num2str(report),' \n']);  
-       else
-         fprintf('report = unknown \n');
-       end	      
-     else
-       report = val;
-     end
+      case 'report'
+	    if exist('val','var') == 0
+	      if numel(report) ~= 0
+	        fprintf(['report = ', num2str(report),' \n']);  
+	      else
+            fprintf('report = unknown \n');
+	      end	      
+	    else
+	      report = val;
+	    end
 
-   case 3 % option 'max_step_number'
-     if exist('val', 'var') == 0
-       if prod(size(max_step_number)) == 1
-	     fprintf(['max_step_number = ', num2str(max_step_number),' \n']);  
-       else
-         fprintf('max_step_number = unknown \n');
-       end	      
-     else
-       max_step_number = val;
-     end
+      case 'max_step_number'
+	    if ~exist('val','var')
+	      if numel(max_step_number) ~= 0
+	        fprintf(['max_step_number = ', num2str(max_step_number),' \n']);  
+	      else
+            fprintf('max_step_number = unknown \n');
+	      end	      
+	    else
+	      max_step_number = val;
+	    end
 
-   case 4 % option 'max_fun_evals'
-     if exist('val', 'var') == 0
-       if prod(size(max_fun_evals)) == 1
-	     fprintf(['max_fun_evals = ', num2str(max_fun_evals),' \n']);
-       else
-	     fprintf('max_fun_evals = unkown \n');
-       end
-     else
-       max_fun_evals = val;
-     end
+      case 'max_fun_evals'
+	    if ~exist('val','var')
+	      if numel(max_fun_evals) ~= 0
+	        fprintf(['max_fun_evals = ', num2str(max_fun_evals),' \n']);
+	      else
+	        fprintf('max_fun_evals = unkown \n');
+	      end
+	    else
+	      max_fun_evals = val;
+	    end
 
-   case 5 % option 'tol_simplex'
-     if exist('val', 'var') == 0
-       if prod(size(tol_simplex)) == 1
-	     fprintf(['tol_simplex = ', num2str(tol_simplex),' \n']);
-       else
-	     fprintf('tol_simplex = unknown \n');
-       end
-     else
-       tol_simplex = val;
-     end
+      case 'tol_simplex'
+	    if ~exist('val','var')
+	      if numel(tol_simplex) ~= 0
+	        fprintf(['tol_simplex = ', num2str(tol_simplex),' \n']);
+	      else
+	        fprintf('tol_simplex = unknown \n');
+	      end
+	    else
+	      tol_simplex = val;
+	    end
 	    
-   case 6 % option 'tol_fun'
-     if exist('val', 'var') == 0
-       if prod(size(tol_fun)) == 1
-	     fprintf(['tol_fun = ', num2str(tol_fun),' \n']);
-       else
-	     fprintf('tol_fun = unknown \n');
-       end
-     else
-       tol_fun = val;
+      case 'tol_fun'
+	    if ~exist('val','var') 
+	      if numel(tol_fun) ~= 0
+	        fprintf(['tol_fun = ', num2str(tol_fun),' \n']);
+	      else
+	        fprintf('tol_fun = unknown \n');
+	      end
+	    else
+	      tol_fun = val;
+        end
+
+      case 'simplex_size'
+	    if ~exist('val','var') 
+	      if numel(simplex_size) ~= 0
+	        fprintf(['simplex_size = ', num2str(simplex_size),' \n']);
+	      else
+	        fprintf('simplex_size = unknown \n');
+	      end
+	    else
+	      simplex_size = val;
+        end
+        
+      otherwise
+        if ~strcmp(key, 'inexistent')   
+            fprintf(['key ', key, ' is unkown \n\n']);
+        end
+	    if numel(report) ~= 0
+          fprintf(['report = ', num2str(report),' \n']);
+	    else
+	      fprintf('report = unknown \n');
+	    end	      
+	    if numel(max_step_number) ~= 0
+	      fprintf(['max_step_number = ', num2str(max_step_number),' \n']);
+	    else
+	      fprintf('max_step_number = unkown \n');
+	    end
+	    if numel(max_fun_evals) ~= 0
+	      fprintf(['max_fun_evals = ', num2str(max_fun_evals),' \n']);
+	    else
+	      fprintf('max_fun_evals = unkown \n');
+	    end
+	    if numel(tol_simplex) ~= 0
+	      fprintf(['tol_simplex = ', num2str(tol_simplex),' \n']);
+	    else
+	      fprintf('tol_simplex = unknown \n');
+	    end
+	    if numel(tol_fun) ~= 0
+	      fprintf(['tol_fun = ', num2str(tol_fun),' \n']);
+	    else
+	      fprintf('tol_fun = unknown \n');
+	    end
+	    if numel(simplex_size) ~= 0
+	      fprintf(['simplex_size = ', num2str(simplex_size),' \n']);
+	    else
+	      fprintf('simplex_size = unknown \n');
+	    end
      end
-	    
-   otherwise % option 'other'
-     if prod(size(report)) == 1
-       fprintf(['report = ', num2str(report),' \n']);
-     else
-       printf('report = unknown \n');
-     end	      
-     if prod(size(max_step_number)) == 1
-        fprintf(['max_step_number = ', num2str(max_step_number),' \n']);
-     else
-        fprintf('max_step_number = unkown \n');
-     end
-     if prod(size(max_fun_evals)) == 1
-        fprintf(['max_fun_evals = ', num2str(max_fun_evals),' \n']);
-     else
-        fprintf('max_fun_evals = unkown \n');
-     end
-     if prod(size(tol_simplex)) == 1
-        fprintf(['tol_simplex = ', num2str(tol_simplex),' \n']);
-     else
-        fprintf('tol_simplex = unknown \n');
-     end
-     if prod(size(tol_fun)) == 1
-        fprintf(['tol_fun = ', num2str(tol_fun),' \n']);
-     else
-       fprintf('tol_fun = unknown \n');
-     end
-   end
